b9e7fa55d3a9636bd29c3bb68b9bbca14c4c6ffb
RenXusheng233 <782449909@qq.com>
2023-01-30 16:10:35 +0800

    feat: nest HMR & dynamic module

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -1,19 +1,34 @@
 import Next from 'next';
-import { Module } from '@nestjs/common';
+import { Module, DynamicModule } from '@nestjs/common';
 import { RenderModule } from 'nest-next';
 import { NODE_ENV } from 'src/shared/constants/env';
 import { BlogModule } from './modules/blog/blog.module';
 import { HomeModule } from './modules/home/home.module';
 
-@Module({
-  /* should pass a NEXT.js server instance
-        as the argument to `forRootAsync` */
-  imports: [
-    RenderModule.forRootAsync(Next({ dev: NODE_ENV === 'development' }), {
-      viewsDir: null,
-    }),
-    HomeModule,
-    BlogModule,
-  ],
-})
-export class AppModule {}
+declare const module: any;
+
+@Module({})
+export class AppModule {
+  public static initialize(): DynamicModule {
+    /* during initialization attempt pulling cached RenderModule
+            from persisted data */
+    const renderModule =
+      module.hot?.data?.renderModule ??
+      RenderModule.forRootAsync(Next({ dev: NODE_ENV === 'development' }), {
+        viewsDir: null,
+      });
+
+    if (module.hot) {
+      /* add a handler to cache RenderModule
+                before disposing existing module */
+      module.hot.dispose((data: any) => {
+        data.renderModule = renderModule;
+      });
+    }
+
+    return {
+      module: AppModule,
+      imports: [renderModule, BlogModule, HomeModule],
+    };
+  }
+}
--- src/server/main.ts
+++ src/server/main.ts
@@ -3,9 +3,16 @@ import { AppModule } from './app.module';
 import { PORT } from 'src/shared/constants/env';
 import { IgnoreFaviconMiddleware } from './common/middlewares/ignore-favicon';
 
+declare const module: any;
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule.initialize());
   app.use(IgnoreFaviconMiddleware);
   await app.listen(PORT);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
