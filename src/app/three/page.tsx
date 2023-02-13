6b0e9ed606f5c7b790db06e253d68d278daa06d6
RenXusheng233 <782449909@qq.com>
2023-02-13 18:42:01 +0800

    chore: code opt

--- src/app/three/page.tsx
+++ src/app/three/page.tsx
@@ -1,26 +1,37 @@
 'use client';
-import * as THREE from 'three';
+import {
+  Scene,
+  PerspectiveCamera,
+  WebGLRenderer,
+  BoxGeometry,
+  MeshBasicMaterial,
+  Mesh,
+} from 'three';
 import { useEffect, useRef } from 'react';
 
 const Three = () => {
   const ref = useRef<HTMLDivElement>();
 
   const createExample = () => {
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(
+    const scene = new Scene();
+
+    const camera = new PerspectiveCamera(
       75,
       window.innerWidth / window.innerHeight,
       0.1,
       1000,
     );
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.position.z = 2;
+
+    const renderer = new WebGLRenderer();
+    renderer.setSize(window.innerWidth / 2, window.innerHeight / 2, false);
     ref.current.appendChild(renderer.domElement);
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
+
+    const geometry = new BoxGeometry(1, 0.8, 0.6);
+    const material = new MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new Mesh(geometry, material);
+
     scene.add(cube);
-    camera.position.z = 5;
 
     const animate = function () {
       requestAnimationFrame(animate);
